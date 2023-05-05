29ae6a2e8f062541e3eed4df4b2af2a0242d2190
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-05 14:32:42 +0300

    Created a face detection endpoint

--- app/main.py
+++ app/main.py
@@ -23,3 +23,26 @@ async def home() -> dict[str, str]:
         dict[str, str]: _description_
     """
     return {"Hello": "World"}
+
+
+@app.post("/face-detection", response_model=Faces)
+async def face_detection(image: UploadFile = File(...)) -> Faces:
+    """An endpoint for uploading photos for facial detection
+
+    Args:
+        image (UploadFile, optional): The image uploaded to the server. Defaults to File(...).
+
+    Returns:
+        Faces: The template for the Face data
+    """
+    data = np.fromfile(image.file, dtype=np.uint8)
+    image = cv2.imdecode(data, cv2.IMREAD_UNCHANGED)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = cascade_classifier.detectMultiScale(gray)
+
+    if len(faces) > 0:
+        faces_output = Faces(faces=faces.tolist())
+    else:
+        faces_output = Faces(faces=[])
+
+    return faces_output
