5be58c283e4d16b6dd4c8f5557a1dabc7ebf792e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-05 14:22:33 +0300

    The basic setup code for the entrypoint for the backend application

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
 """The entrypoint to the application"""
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/", name="Home", description="The home endpoint for the backend application", tags=['Home'])
+async def home() -> dict[str, str]:
+    """The home endpoint for the backend application
+
+    Returns:
+        dict[str, str]: _description_
+    """
+    return {"Hello": "World"}
