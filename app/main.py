86aa1ab754a52b0849287a271cb364e02d54a3a7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-05 14:27:45 +0300

    Made a Model for faces and created an instance of the cascade_classifeie function

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 """The entrypoint to the application"""
-from fastapi import FastAPI
+from typing import List, Tuple
 
+import cv2
+import numpy as np
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
 
 app = FastAPI()
+cascade_classifier = cv2.CascadeClassifier()
+
+
+class Faces(BaseModel):
+    """The model for the faces"""
+    faces: List[Tuple[int, int, int, int]]
 
 
 @app.get("/", name="Home", description="The home endpoint for the backend application", tags=['Home'])
